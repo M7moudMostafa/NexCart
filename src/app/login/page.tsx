f2d2a2819e8bf7618231cf585cdfe6f586df56a7
Mahmoud Mostafa <156802364+M7moudMostafa@users.noreply.github.com>
2025-11-10 19:48:29 +0200

    Add Login Logic

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+import { useWixClient } from "@/hooks/useWixClient";
+import { LoginState } from "@wix/sdk";
+import Cookies from "js-cookie";
+import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
 
 enum MODE {
@@ -10,6 +14,16 @@ enum MODE {
 }
 
 const LoginPage = () => {
+    const wixClient = useWixClient();
+    const pathName = usePathname();
+    const router = useRouter();
+
+    const isLoggedIn = wixClient.auth.loggedIn();
+
+    if (isLoggedIn) {
+        router.push("/");
+    }
+
     const [mode, setMode] = useState<MODE>(MODE.LOGIN);
 
     const [userName, setUserName] = useState<string>("");
@@ -38,9 +52,91 @@ const LoginPage = () => {
                     ? "Reset"
                     : "Verify";
 
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        setIsLoading(true);
+        setError("");
+
+        try {
+            let response;
+
+            switch (mode) {
+                case MODE.LOGIN:
+                    response = await wixClient.auth.login({
+                        email,
+                        password,
+                    });
+                    break;
+                case MODE.REGISTER:
+                    response = await wixClient.auth.register({
+                        email,
+                        password,
+                        profile: { nickname: userName },
+                    });
+                    break;
+                case MODE.RESET_PASSWORD:
+                    response = await wixClient.auth.sendPasswordResetEmail(
+                        email,
+                        pathName
+                    );
+                    setMessage("Password reset email sent. Please check your inbox.");
+                    break;
+                case MODE.EMAIL_VERIFICATION:
+                    response = await wixClient.auth.processVerification({
+                        verificationCode: emailCode,
+                    });
+                    break;
+                default:
+                    break;
+            }
+
+            switch (response?.loginState) {
+                case LoginState.SUCCESS:
+                    setMessage("Successfully! You are being redircted.");
+                    const tokens = await wixClient.auth.getMemberTokensForDirectLogin(
+                        response.data.sessionToken
+                    );
+                    console.log(tokens);
+                    Cookies.set("refreshToken", JSON.stringify(tokens.refreshToken), {
+                        expires: 2,
+                    });
+                    wixClient.auth.setTokens(tokens);
+                    router.push("/");
+                    break;
+                case LoginState.FAILURE:
+                    if (
+                        response?.errorCode === "invalidEmail" ||
+                        response?.errorCode === "invalidPassword"
+                    ) {
+                        setError("Invalid email or password.");
+                    } else if (response?.errorCode === "emailAlreadyExists") {
+                        setError("Email already exists.");
+                    } else if (response?.errorCode === "resetPassword") {
+                        setMessage("Password reset email sent. Please check your inbox.");
+                    } else {
+                        setError("An error occurred. Please try again.");
+                    }
+                    break;
+                case LoginState.EMAIL_VERIFICATION_REQUIRED:
+                    setMode(MODE.EMAIL_VERIFICATION);
+                    setMessage("Please enter the verification code sent to your email.");
+                    break;
+                case LoginState.OWNER_APPROVAL_REQUIRED:
+                    setMessage("Your account is pending owner approval.");
+                    break;
+                default:
+                    break;
+            }
+        } catch (error) {
+            setError("An error occurred. Please try again.");
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
     return (
         <div className="h-[calx(100vh-80px)] px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64 flex justify-center items-center">
-            <form action="" className="flex flex-col gap-8">
+            <form action="" className="flex flex-col gap-8" onSubmit={handleSubmit}>
                 <h1 className="text-2sx font-semibold">{formTitle}</h1>
                 {mode === MODE.REGISTER ? (
                     <div className="flex flex-col gap-2">
@@ -50,6 +146,9 @@ const LoginPage = () => {
                             name="username"
                             placeholder="john"
                             className="ring-2 ring-gray-300 rounded-md p-4"
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setUserName(e.target.value)
+                            }
                         />
                     </div>
                 ) : null}
@@ -61,6 +160,9 @@ const LoginPage = () => {
                             name="email"
                             placeholder="john@mail.com"
                             className="ring-2 ring-gray-300 rounded-md p-4"
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setEmail(e.target.value)
+                            }
                         />
                     </div>
                 ) : (
@@ -71,6 +173,9 @@ const LoginPage = () => {
                             name="emailCode"
                             placeholder="Code"
                             className="ring-2 ring-gray-300 rounded-md p-4"
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setEmailCode(e.target.value)
+                            }
                         />
                     </div>
                 )}
@@ -82,10 +187,20 @@ const LoginPage = () => {
                             name="password"
                             placeholder="Enter your password"
                             className="ring-2 ring-gray-300 rounded-md p-4"
+                            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                                setPassword(e.target.value)
+                            }
                         />
                     </div>
                 ) : null}
-                {mode === MODE.LOGIN && <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.RESET_PASSWORD)}>Forgot Password?</div>}
+                {mode === MODE.LOGIN && (
+                    <div
+                        className="text-sm underline cursor-pointer"
+                        onClick={() => setMode(MODE.RESET_PASSWORD)}
+                    >
+                        Forgot Password?
+                    </div>
+                )}
                 <button
                     className="bg-demo text-white p-2 rounded-md disabled:bg-pink-200 disabled:cursor-not-allowed"
                     disabled={isLoading}
@@ -94,17 +209,26 @@ const LoginPage = () => {
                 </button>
                 {error && <div className="text-red-600">{error}</div>}
                 {mode === MODE.LOGIN && (
-                    <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.REGISTER)}>
+                    <div
+                        className="text-sm underline cursor-pointer"
+                        onClick={() => setMode(MODE.REGISTER)}
+                    >
                         {"Don't"} have an account? Register
                     </div>
                 )}
                 {mode === MODE.REGISTER && (
-                    <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.LOGIN)}>
+                    <div
+                        className="text-sm underline cursor-pointer"
+                        onClick={() => setMode(MODE.LOGIN)}
+                    >
                         Have an account?
                     </div>
                 )}
                 {mode === MODE.RESET_PASSWORD && (
-                    <div className="text-sm underline cursor-pointer" onClick={() => setMode(MODE.LOGIN)}>
+                    <div
+                        className="text-sm underline cursor-pointer"
+                        onClick={() => setMode(MODE.LOGIN)}
+                    >
                         Go back to Login
                     </div>
                 )}
